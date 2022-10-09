74b689e36ad6858e3169ba71d36f8710dd7c44cc
sahib2442 <110272016+sahib2442@users.noreply.github.com>
2022-10-09 10:24:53 +0530

    Update AddressBook.cs

--- AddressBookSystem/AddressBook.cs
+++ AddressBookSystem/AddressBook.cs
@@ -8,15 +8,7 @@ namespace AddressBookSystem
         public static void AddContacts()
         {
             Console.WriteLine("\n select program \n 1.FirstName 2.LastName 3.Address 4.City 5.State 6.Zip 7.PhoneNo 8.Email");
-            AddressBook addressBook = new AddressBook();
-            while (addressBook == null)
-            {
-                Console.WriteLine("\n select program \n 1.AddPerson 2.EditPerson 3.DeletePerson");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
-                        Contact contact = new Contact();
+            Contact contact = new Contact();
                         {
                             Console.WriteLine("Enter first name");
                             contact.FirstName = Console.ReadLine();
@@ -29,38 +21,29 @@ namespace AddressBookSystem
                             Console.WriteLine("Enter state");
                             contact.State = Console.ReadLine();
                             contact.Addperson(contact);
-                            break;
                         }
-                    case 2:
-                        {
-                            Console.WriteLine("Enter first name");
-                            contact.FirstName = Console.ReadLine();
-                            Console.WriteLine("Enter last name");
-                            contact.LastName = Console.ReadLine();
-                            Console.WriteLine("Enter mobile number");
-                            contact.PhoneNumber = Console.ReadLine();
-                            Console.WriteLine("Enter Address");
-                            contact.Address = Console.ReadLine();
-                            Console.WriteLine("Enter state");
-                            contact.State = Console.ReadLine();
-                            contact.EditPerson(contact);
-                            break;
-                        }
+            AddressBook addressBook = new AddressBook();
+            while (addressBook == null)
+            {
+                Console.WriteLine("\n select program \n 1.AddPerson 2.EditPerson 3.DeletePerson");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        add();
+			        	break;
+			        case 2:
+                        edit();
+				        break;
                     case 3:
-                        {
-                            Console.WriteLine("Enter first name");
-                            contact.FirstName = Console.ReadLine();
-                            Console.WriteLine("Enter last name");
-                            contact.LastName = Console.ReadLine();
-                            Console.WriteLine("Enter mobile number");
-                            contact.PhoneNumber = Console.ReadLine();
-                            Console.WriteLine("Enter Address");
-                            contact.Address = Console.ReadLine();
-                            Console.WriteLine("Enter state");
-                            contact.State = Console.ReadLine();
-                            contact.DeleteContact(contact);
-                            break;
-                        }
+                        delete();
+				        break;
+                    case 5:
+                        print();
+				        break;
+                    case 6:
+                        status = false;
+				        break;
                 }
             }
         }
